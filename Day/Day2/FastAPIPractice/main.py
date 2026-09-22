344c9fbf7669b665e27a1e1c6a59302b76bf59c4
Basavaraj <basuchikkamath5@gmail.com>
2026-09-22 13:21:16 +0530

    Path Parameters

--- Day/Day2/FastAPIPractice/main.py
+++ Day/Day2/FastAPIPractice/main.py
@@ -15,3 +15,7 @@ def health():
 @app.post("/create")
 def create_something():
     return {"message":"Created"}
+#Path Parameters
+@app.get("/student/{usn}")
+def get_result(usn):
+    return {"Result":"Distinction","usn":usn}
