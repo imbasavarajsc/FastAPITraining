ffa73483ac0753ecca9162c6682bf7321cb00754
Basavaraj <basuchikkamath5@gmail.com>
2026-09-22 15:19:17 +0530

    Pydantic model

--- Day/Day2/FastAPIPractice/main.py
+++ Day/Day2/FastAPIPractice/main.py
@@ -1,26 +1,34 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
 app = FastAPI()
 @app.get("/")
 def home():
-    return {"page":"Home"}
+    return{"page":"Home"}
 @app.get("/about")
 def about():
-    return{"page":"About","author":"Basavaraj"}
-
+    return{"page":"About","author":"Rakesh"}
 @app.get("/health")
 def health():
-    return {"status":"ok"}
-
-#Post Request
+    return {"Status":"ok"}
+#postreq
 @app.post("/create")
-def create_something():
-    return {"message":"Created"}
-#Path Parameters
-@app.get("/student/{rollno}")
-def get_result(rollno):
-    return {"Result":"Distinction","rollno":rollno}
+def create_some():
+    return {"messege":"Created"}
 
-#Path Parameters with Type Hint 
+
+#path
+@app.get("/student/{usn}")
+def get_result(usn):
+    return {"Result ":"Distinction","USN":usn}
+
 @app.get("/candidate/{rollno}")
-def get_candidate(rollno:int):
-    return {"Result":"Distinction","rollno":rollno,"type":str(type(rollno))}
+def get_date(rollno:int):
+    return {"Result ":"Distinction","roll":rollno,"type":str(type(rollno))}
+
+class Item(BaseModel):
+    name : str
+    price : float
+    in_stock : bool = True
+@app.post("/items")   
+def create_item(item : Item):
+    return {"received":item,"total_price":item.price*1.18}
